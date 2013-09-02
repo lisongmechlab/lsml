938c8d704cba53912608ed14f6c0e5eb7253faf4
J Scarrott <johnps@outlook.com>
2013-09-02 11:34:38 +0100

    Ammo Table now shows correct values and updates properly. Need to write unit tests.

--- src/lisong_mechlab/model/tables/AmmoTableDataModel.java
+++ src/lisong_mechlab/model/tables/AmmoTableDataModel.java
@@ -5,42 +5,72 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.TreeMap;
 
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
+import lisong_mechlab.model.MessageXBar;
+import lisong_mechlab.model.MessageXBar.Message;
+import lisong_mechlab.model.item.Item;
 import lisong_mechlab.model.loadout.Loadout;
+import lisong_mechlab.model.loadout.LoadoutPart;
 import lisong_mechlab.model.loadout.metrics.TotalAmmoSupply;
 
-public class AmmoTableDataModel implements TableModel{
+public class AmmoTableDataModel implements TableModel, MessageXBar.Reader{
    
    private final List<TableModelListener>                     listeners = new ArrayList<TableModelListener>();
    
    protected String weaponNames;
    private Loadout aLoadout;
    private TotalAmmoSupply totalAmmoSupply;
-   private Object[][] data = {{"srm"}, {"345"}};
-   private String[] columnNames = {"Weapon" , "Ammo"};
+   private Object[][] data = {{"srm"}, {"345"}};//Exception occurs if there arn't some values.
+   private String[] columnNames = {"Weapon" , "Ammo", "Volley Amount" , "Number of Volleys"};
    
-   public AmmoTableDataModel(Loadout aloadout){
+   public AmmoTableDataModel(Loadout aloadout, MessageXBar aXBar){
       this.aLoadout = aloadout;
       totalAmmoSupply = new TotalAmmoSupply(aLoadout);
       totalAmmoSupply.calculate();
-//      data = new Object[totalAmmoSupply.calculate().size()][2];
-//      fillInData();
+      aXBar.attach(this);
+
    }
    
    public void fillInData(){
-      data = new Object[totalAmmoSupply.calculate().size()][2];
-      Set<Entry<String, Integer>> entrySet = totalAmmoSupply.calculate().entrySet();
-      Iterator<Entry<String, Integer>> entryIterator = entrySet.iterator();
+      data = new Object[totalAmmoSupply.calculate().size()][4];
+      Set<Entry<Item, Integer>> entrySet = totalAmmoSupply.calculate().entrySet();
+      Iterator<Entry<Item, Integer>> entryIterator = entrySet.iterator();
       int entryCounter = 0;
       while( entryIterator.hasNext()){
-         Entry<String, Integer> entryTemp = entryIterator.next();
-          data[entryCounter][0] = entryTemp.getKey();
+         Entry<Item, Integer> entryTemp = entryIterator.next();
+            data[entryCounter][0] = entryTemp.getKey().getName();
           data[entryCounter][1] = entryTemp.getValue();
           entryCounter++;
+      }
+      fillInVolleyData();
+      fillInTotalNumberOfVolleys();
+   }
+   
+   public void fillInVolleyData(){
+      
+      TreeMap<String, Integer> volleyMap = (TreeMap<String, Integer>)totalAmmoSupply.getShotsPerVolleyForEach();
+      Integer[] volleyArray = volleyMap.values().toArray(new Integer[volleyMap.values().size()]);
+      for(int i = 0; i < data.length; i++){
+         data[i][2] = volleyArray[i];
+      }
+      
+   }
+   
+   public void fillInTotalNumberOfVolleys(){
+      for(int i = 0; i < data.length; i++){
+         int allAmmo =  (int)data[i][1];
+         int volleyValue = (int)data[i][2];
+         if(volleyValue != 0){
+                     data[i][3] = allAmmo/volleyValue;
+         }
+         else{
+            data[i][3] = 0;
+         }
       }
    }
 
@@ -57,7 +87,11 @@ public class AmmoTableDataModel implements TableModel{
    }
    @Override
    public Class<?> getColumnClass(int aColumnIndex){
-      return getValueAt(0, aColumnIndex).getClass();
+      if(aColumnIndex == 0){
+         return String.class;
+      }
+      else
+         return Integer.class;
    }
 
    @Override
@@ -95,6 +129,15 @@ public class AmmoTableDataModel implements TableModel{
    @Override
    public void setValueAt(Object aValue, int aRowIndex, int aColumnIndex){
       // TODO Auto-generated method stub
+      
+   }
+
+   @Override
+   public void receive(Message aMsg){
+      if(aMsg instanceof LoadoutPart.Message){
+         totalAmmoSupply = new TotalAmmoSupply(aLoadout);
+         fillInData();
+      }
       
    }
 
