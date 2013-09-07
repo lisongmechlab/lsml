fa44e5b1ac0e6b32cc4c8726f1e8d49d80b92c31
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2013-09-07 14:49:24 +0200

    Fixing issue #77. Added a beep when attempting to go over tonnage on armor. Updated unit tests. Refactored some unit tests to make them shorter and more concise using a new helper class.

--- src/lisong_mechlab/view/ArmorSpinner.java
+++ src/lisong_mechlab/view/ArmorSpinner.java
@@ -1,5 +1,7 @@
 package lisong_mechlab.view;
 
+import java.awt.Toolkit;
+
 import javax.swing.AbstractSpinnerModel;
 import javax.swing.SwingUtilities;
 
@@ -7,18 +9,19 @@ import lisong_mechlab.model.MessageXBar;
 import lisong_mechlab.model.MessageXBar.Message;
 import lisong_mechlab.model.chassi.ArmorSide;
 import lisong_mechlab.model.loadout.LoadoutPart;
+import lisong_mechlab.model.loadout.LoadoutPart.Message.Type;
 
 public class ArmorSpinner extends AbstractSpinnerModel implements MessageXBar.Reader{
    private static final long serialVersionUID = 2130487332299251881L;
    private final LoadoutPart part;
-   private final ArmorSide side;
+   private final ArmorSide   side;
 
    public ArmorSpinner(LoadoutPart aPart, ArmorSide anArmorSide, MessageXBar anXBar){
       part = aPart;
       side = anArmorSide;
       anXBar.attach(this);
    }
-   
+
    @Override
    public Object getNextValue(){
       if( part.getArmor(side) < part.getArmorMax(side) ){
@@ -41,19 +44,29 @@ public class ArmorSpinner extends AbstractSpinnerModel implements MessageXBar.Re
 
    @Override
    public void setValue(Object arg0){
-      part.setArmor(side, ((Integer)arg0).intValue());
-      fireStateChanged();
+      try{
+         part.setArmor(side, ((Integer)arg0).intValue());
+         fireStateChanged();
+      }
+      catch( IllegalArgumentException exception ){
+         // TODO: Show message in status bar
+         Toolkit.getDefaultToolkit().beep();
+      }
    }
 
    @Override
    public void receive(Message aMsg){
-      // TODO Be more selective!
-      SwingUtilities.invokeLater(new Runnable(){
-         @Override
-         public void run(){
-            fireStateChanged();            
+      if( aMsg instanceof LoadoutPart.Message ){
+         LoadoutPart.Message message = (LoadoutPart.Message)aMsg;
+         if( message.part == part && message.type == Type.ArmorChanged ){
+            SwingUtilities.invokeLater(new Runnable(){
+               @Override
+               public void run(){
+                  fireStateChanged();
+               }
+            });
          }
-      });      
+      }
    }
 
 }
