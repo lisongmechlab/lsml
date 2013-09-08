e0c3333227311e3bbfc8863f798bbb7b5897b5ce
Emily Björk <bjoerk.emily@gmail.com>
2013-09-08 21:59:01 +0200

    Fixing issue #90

--- src/lisong_mechlab/model/loadout/metrics/AlphaStrike.java
+++ src/lisong_mechlab/model/loadout/metrics/AlphaStrike.java
@@ -16,7 +16,7 @@ public class AlphaStrike extends Metric{
       double ans = 0;
       for(Item item : loadout.getAllItems()){
          if( item instanceof Weapon ){
-            ans += ((Weapon)item).getDamagePerVolley();
+            ans += ((Weapon)item).getDamagePerShot();
          }
       }
       return ans;
