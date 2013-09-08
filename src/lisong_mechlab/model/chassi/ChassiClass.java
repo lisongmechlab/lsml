247e492905fceb16af5642c2fa5aa7de015a773a
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2013-09-08 14:00:57 +0200

    Fixing issue #84. Updated unit tests.

--- src/lisong_mechlab/model/chassi/ChassiClass.java
+++ src/lisong_mechlab/model/chassi/ChassiClass.java
@@ -3,25 +3,28 @@ package lisong_mechlab.model.chassi;
 public enum ChassiClass{
    LIGHT(1.4), MEDIUM(1.3), HEAVY(1.2), ASSAULT(1.2);
 
+   private final static double TONNAGE_EPSILON = Math.ulp(100) * 5.0;
+
    ChassiClass(double aMultiplier){
       multiplier = aMultiplier;
    }
 
    public static ChassiClass fromMaxTons(double tons){
-      if( tons <= 35 ){
+
+      if( tons < 40 - TONNAGE_EPSILON ){
          return ChassiClass.LIGHT;
       }
-      else if( tons <= 55 ){
+      else if( tons < 60 - TONNAGE_EPSILON ){
          return ChassiClass.MEDIUM;
       }
-      else if( tons <= 70 ){
+      else if( tons < 80 - TONNAGE_EPSILON ){
          return ChassiClass.HEAVY;
       }
       else{
          return ChassiClass.ASSAULT;
       }
    }
-   
+
    public double EngineMultiplier(){
       return multiplier;
    }
