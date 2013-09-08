960cce67788bdf91f6d2ded8a4fd5834d66cdffd
EmilyBjoerk <bjoerk.emily@gmail.com>
2013-09-08 21:29:11 +0200

    Adding the actual files. Also updated documentation and unit test.

---
+++ test/lisong_mechlab/model/loadout/metrics/AlphaStrikeTest.java
@@ -0,0 +1,43 @@
+package lisong_mechlab.model.loadout.metrics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lisong_mechlab.model.helpers.MockLoadoutContainer;
+import lisong_mechlab.model.item.Item;
+import lisong_mechlab.model.item.ItemDB;
+import lisong_mechlab.model.item.Weapon;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+
+/**
+ * Test suite for {@link AlphaStrike}.
+ * 
+ * @author Emily Björk
+ */
+public class AlphaStrikeTest{
+   private final MockLoadoutContainer mlc = new MockLoadoutContainer();
+   private final AlphaStrike          cut = new AlphaStrike(mlc.loadout);
+
+   /**
+    * Calculate shall sum up the per volley damage of all weapons.
+    */
+   @Test
+   public void testCalculate(){
+      List<Item> items = new ArrayList<>();
+      Weapon ppc = (Weapon)ItemDB.lookup("PPC");
+      Weapon ll = (Weapon)ItemDB.lookup("LARGE LASER");
+      Weapon lrm20 = (Weapon)ItemDB.lookup("LRM 20");
+      Weapon lb10x = (Weapon)ItemDB.lookup("LB 10-X AC");
+      items.add(ItemDB.BAP); // Shall not barf on non-weapons
+      items.add(ppc);
+      items.add(ll);
+      items.add(lrm20);
+      items.add(lb10x);
+      when(mlc.loadout.getAllItems()).thenReturn(items);
+
+      double expected = ppc.getDamagePerVolley() + ll.getDamagePerVolley() + lrm20.getDamagePerVolley() + lb10x.getDamagePerVolley();
+      assertEquals(expected, cut.calculate(), 0.0);
+   }
+}
---
+++ test/lisong_mechlab/model/loadout/metrics/TimeToOverHeatTest.java
@@ -0,0 +1,58 @@
+package lisong_mechlab.model.loadout.metrics;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * A test suite for {@link TimeToOverHeat}.
+ * 
+ * @author Emily Björk
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TimeToOverHeatTest{
+   @Mock
+   private HeatCapacity    capacity;
+   @Mock
+   private HeatDissipation dissipation;
+   @Mock
+   private HeatGeneration  generation;
+
+   @InjectMocks
+   private TimeToOverHeat  cut;
+
+   /**
+    * 15 minutes and above is rounded up to infinity. Matches are only 15 minutes :)
+    */
+   @Test
+   public void testGetTimeToOverHeat_15minutes(){
+      final double heat = 10;
+      final double cooling = heat - 1;
+      final double ccapacity = 15 * 60;
+      when(generation.calculate()).thenReturn(heat);
+      when(dissipation.calculate()).thenReturn(cooling);
+      when(capacity.calculate()).thenReturn(ccapacity);
+
+      assertEquals(Double.POSITIVE_INFINITY, cut.calculate(), 0);
+   }
+
+   /**
+    * If a mech generates 10 heat per second and can dissipate 5, then the mech will over heat after the differential
+    * has filled the heat capacity: capacity / (generation - dissipation)
+    */
+   @Test
+   public void testGetTimeToOverHeat(){
+      double heat = 10;
+      double cooling = 5;
+      double ccapacity = 60;
+      when(generation.calculate()).thenReturn(heat);
+      when(dissipation.calculate()).thenReturn(cooling);
+      when(capacity.calculate()).thenReturn(ccapacity);
+      assertEquals(ccapacity / (heat - cooling), cut.calculate(), 0);
+   }
+}
