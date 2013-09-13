f2f37dd3bf3bf62d406174077d8162812d777e8d
J Scarrott <johnps@outlook.com>
2013-09-13 22:06:00 +0100

    Added check artemis legal to artemisHandler

--- src/lisong_mechlab/model/loadout/ArtemisHandler.java
+++ src/lisong_mechlab/model/loadout/ArtemisHandler.java
@@ -1,5 +1,6 @@
 package lisong_mechlab.model.loadout;
 
+import lisong_mechlab.model.chassi.Chassi;
 import lisong_mechlab.model.item.Ammunition;
 import lisong_mechlab.model.item.Item;
 import lisong_mechlab.model.item.MissileWeapon;
@@ -7,39 +8,62 @@ import lisong_mechlab.model.item.MissileWeapon;
 public class ArtemisHandler {
 	
 	private Loadout loadout;
+	private Chassi  chassi;
+	private int additionalCritSlots;
+	private int additionalMass;
 
 	public ArtemisHandler(Loadout loadoutUnderTest) {
 		loadout = loadoutUnderTest;
+		chassi = loadout.getChassi();
+	    additionalCritSlots = 0;
+	      additionalMass = 0;
 	}
 	
 	
 	
-	public boolean checkLoadoutStillValid(){
-		
-		for(LoadoutPart part : loadout.getPartLoadOuts()){
-			if(!checkPartCanHoldArtemis(part)) {
-				return false;
-			}
+	public void checkLoadoutStillValid(){
+		if(!loadout.getUpgrades().hasArtemis()){
+		   for(LoadoutPart part : loadout.getPartLoadOuts()){
+	         if(!checkPartCanHoldArtemis(part)) {
+	            throw new IllegalArgumentException("Not enough free crit slots!");
+	         }
+	      } 
 		}
-		return true;
+		
+		
 	}
 
 
 
 	private boolean checkPartCanHoldArtemis(LoadoutPart aPart) {
-		int additionalCritSlots = 0;
+
 		for(Item item : aPart.getItems()){
 			if((item instanceof MissileWeapon) ){
 				additionalCritSlots++;
+				additionalMass++;
 			}
 
 		}
-		System.out.println(aPart.getNumCriticalSlotsFree());
 		if(((aPart.getNumCriticalSlotsFree()) < additionalCritSlots) && additionalCritSlots != 0){
+		   
 			return false;
 		}
 		return true;
 		
 	}
+	
+	public void checkArtemisAdditionLegal() throws IllegalArgumentException{
+	   if(!loadout.getUpgrades().hasArtemis()){
+	      System.out.println(loadout.getMass() + additionalMass);
+	      System.out.println(loadout.getNumCriticalSlotsFree() - additionalCritSlots);
+	      if( (loadout.getMass() + additionalMass) > chassi.getMassMax() ){
+	         throw new IllegalArgumentException("Not enough free mass!");
+	      }
+	      if( (loadout.getNumCriticalSlotsFree() - additionalCritSlots) < 0 ){
+	         throw new IllegalArgumentException("Not enough free crit slots!");
+	      }
+	   }
+	   
+   }
 
 }
