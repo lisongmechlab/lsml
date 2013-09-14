d4c22551548252861d14a061c72c080fb55ed4d7
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2013-09-14 17:26:08 +0200

    changed error message to be readable

--- test/lisong_mechlab/model/loadout/ArtemisHandlerTest.java
+++ test/lisong_mechlab/model/loadout/ArtemisHandlerTest.java
@@ -1,7 +1,20 @@
 package lisong_mechlab.model.loadout;
 
+import org.junit.Test;
 
+import lisong_mechlab.model.helpers.MockLoadoutContainer;
 
+/**
+ * Tests the {@link ArtemisHandler} class.
+ * 
+ * @author Li Song
+ */
 public class ArtemisHandlerTest{
-
+   private final MockLoadoutContainer mlc = new MockLoadoutContainer();
+   private final ArtemisHandler       cut = new ArtemisHandler(mlc.loadout);
+   
+   @Test
+   public void testCanApplyUpgrade(){
+      
+   }
 }
