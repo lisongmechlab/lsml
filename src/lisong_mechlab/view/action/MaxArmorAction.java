b317faa139396ae7699ff19c9561a23a3d206fa5
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2013-10-26 21:40:55 +0200

    Finishing up the remaining dialogs

--- src/lisong_mechlab/view/action/MaxArmorAction.java
+++ src/lisong_mechlab/view/action/MaxArmorAction.java
@@ -6,6 +6,7 @@ import javax.swing.AbstractAction;
 import javax.swing.JOptionPane;
 
 import lisong_mechlab.model.loadout.Loadout;
+import lisong_mechlab.view.LoadoutFrame;
 
 /**
  * This action sets the armor to max on the given {@link Loadout}.
@@ -13,9 +14,10 @@ import lisong_mechlab.model.loadout.Loadout;
  * @author Li Song
  */
 public class MaxArmorAction extends AbstractAction{
-   private static final long serialVersionUID = -5939335331941199195L;
-   private final Loadout     loadout;
-   private final double      ratio;
+   private static final long  serialVersionUID = -5939335331941199195L;
+   private final Loadout      loadout;
+   private final double       ratio;
+   private final LoadoutFrame loadoutFrame;
 
    /**
     * Creates a new {@link MaxArmorAction}. If <code>aRatio</code> is less than or equal to 0, the user is prompted for
@@ -28,9 +30,10 @@ public class MaxArmorAction extends AbstractAction{
     * @param aRatio
     *           The ratio between back and front as: <code>front/back</code>
     */
-   public MaxArmorAction(String aTitle, Loadout aLoadout, double aRatio){
+   public MaxArmorAction(String aTitle, LoadoutFrame aLoadout, double aRatio){
       super(aTitle);
-      loadout = aLoadout;
+      loadoutFrame = aLoadout;
+      loadout = aLoadout.getLoadout();
       ratio = aRatio;
    }
 
@@ -41,7 +44,8 @@ public class MaxArmorAction extends AbstractAction{
             loadout.setMaxArmor(ratio);
          }
          else{
-            String input = (String)JOptionPane.showInputDialog(null, "Please enter the ratio between front and back armor as front:back.Example 3:1",
+            String input = (String)JOptionPane.showInputDialog(loadoutFrame,
+                                                               "Please enter the ratio between front and back armor as front:back.Example 3:1",
                                                                "Maximizing armor...", JOptionPane.INFORMATION_MESSAGE, null, null, "3:1");
             String[] s = input.split(":");
             if( s.length == 2 ){
@@ -51,20 +55,20 @@ public class MaxArmorAction extends AbstractAction{
                   back = Double.parseDouble(s[1]);
                }
                catch( Exception e ){
-                  JOptionPane.showMessageDialog(null, "Error parsing ratio! Loadout was not changed!");
+                  JOptionPane.showMessageDialog(loadoutFrame, "Error parsing ratio! Loadout was not changed!");
                   return;
                }
                loadout.setMaxArmor(front / back);
             }
             else
-               JOptionPane.showMessageDialog(null, "Error parsing ratio! Loadout was not changed!");
+               JOptionPane.showMessageDialog(loadoutFrame, "Error parsing ratio! Loadout was not changed!");
          }
       }
       catch( IllegalArgumentException e ){
-         JOptionPane.showMessageDialog(null, "Unable to set max armor! Error: " + e.getMessage());
+         JOptionPane.showMessageDialog(loadoutFrame, "Unable to set max armor! Error: " + e.getMessage());
       }
       catch( Throwable e ){
-         JOptionPane.showMessageDialog(null, "Unexpected exception! Error: " + e);
+         JOptionPane.showMessageDialog(loadoutFrame, "Unexpected exception! Error: " + e);
       }
    }
 }
--- src/lisong_mechlab/view/action/RenameLoadoutAction.java
+++ src/lisong_mechlab/view/action/RenameLoadoutAction.java
@@ -8,22 +8,32 @@ import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
 import lisong_mechlab.model.loadout.Loadout;
+import lisong_mechlab.view.LoadoutFrame;
 
 public class RenameLoadoutAction extends AbstractAction{
-   private static final long serialVersionUID = -673375419929455179L;
-   private final Loadout     loadout;
+   private static final long  serialVersionUID = -673375419929455179L;
+   private final LoadoutFrame loadoutFrame;
+   private final Loadout      loadout;
 
    public RenameLoadoutAction(Loadout aLoadout, KeyStroke key){
       super("Rename loadout...");
       loadout = aLoadout;
+      loadoutFrame = null;
+      putValue(Action.ACCELERATOR_KEY, key);
+   }
+
+   public RenameLoadoutAction(LoadoutFrame aLoadoutFrame, KeyStroke key){
+      super("Rename loadout...");
+      loadout = aLoadoutFrame.getLoadout();
+      loadoutFrame = aLoadoutFrame;
       putValue(Action.ACCELERATOR_KEY, key);
    }
 
    @Override
    public void actionPerformed(ActionEvent aE){
-      String name = JOptionPane.showInputDialog("Give a name", loadout.getName());
+      String name = JOptionPane.showInputDialog(loadoutFrame, "Give a name", loadout.getName());
       if( name == null || name.isEmpty() ){
-         JOptionPane.showMessageDialog(null, "No name given!");
+         JOptionPane.showMessageDialog(loadoutFrame, "No name given!");
          return;
       }
       loadout.rename(name);
