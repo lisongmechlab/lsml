908a701be018453c90db49c275a9f8d3e8500bcc
Emily Björk <bjoerk.emily@gmail.com>
2013-12-03 22:44:15 +0100

    Fixing issue #201

--- src/lisong_mechlab/view/LSML.java
+++ src/lisong_mechlab/view/LSML.java
@@ -48,7 +48,7 @@ import lisong_mechlab.view.preferences.Preferences;
  */
 public class LSML extends JFrame{
    public static final String      PROGRAM_FNAME          = "Li Song Mechlab ";
-   private static final String     VERSION_STRING         = "1.2.0";
+   private static final String     VERSION_STRING         = "1.2.1";
    private static final String     GARAGE_FILEDESCRIPTION = PROGRAM_FNAME + " Garage File (.xml)";
    private static final FileFilter GARAGE_FILE_FILTER     = new FileFilter(){
                                                              @Override
