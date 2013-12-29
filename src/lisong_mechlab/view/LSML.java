25be2324643bc07c0a973d26da829f18fc2a1da3
Emily Björk <bjoerk.emily@gmail.com>
2013-12-29 17:32:38 +0100

    Changing version numbers for release.

--- src/lisong_mechlab/view/LSML.java
+++ src/lisong_mechlab/view/LSML.java
@@ -52,7 +52,7 @@ import lisong_mechlab.view.preferences.Preferences;
  */
 public class LSML extends JFrame{
    public static final String      PROGRAM_FNAME          = "Li Song Mechlab";
-   private static final String     VERSION_STRING         = "(develop)";
+   private static final String     VERSION_STRING         = " 1.3.0-RC1";
    private static final String     GARAGE_FILEDESCRIPTION = PROGRAM_FNAME + " Garage File (.xml)";
    private static final FileFilter GARAGE_FILE_FILTER     = new FileFilter(){
                                                              @Override
