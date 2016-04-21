79d170f1d26e6f209cc9f1bf63e1efa5f27cab31
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2016-04-21 17:30:28 +0200

    Makes it so that the window bar of windows will always be inside the screen so that the close button can be accessed. Affects #541.

--- src/main/java/org/lisoft/lsml/view_fx/util/FxControlUtils.java
+++ src/main/java/org/lisoft/lsml/view_fx/util/FxControlUtils.java
@@ -58,7 +58,7 @@ import javafx.stage.StageStyle;
 
 /**
  * Assorted helper methods for dealing with FXML.
- * 
+ *
  * @author Li Song
  */
 public class FxControlUtils {
@@ -66,7 +66,7 @@ public class FxControlUtils {
      * This method facilitates a kind of a three-way binding that may fail. The value of the check-box is bound to a
      * {@link BooleanExpression}, an attempt to change the value of the check-box will be sent to a predicate that may
      * fail. If the predicate fails the check-box retains it's old value.
-     * 
+     *
      * @param aCheckBox
      *            The {@link CheckBox} to bind.
      * @param aBooleanExpression
@@ -94,7 +94,7 @@ public class FxControlUtils {
      * This method facilitates a kind of a three-way binding that may fail. The value of the toggle button is bound to a
      * {@link BooleanExpression}, an attempt to change the value of the toggle button will be sent to a predicate that
      * may fail. If the predicate fails the toggle button retains it's old value.
-     * 
+     *
      * @param aToggleButton
      *            The {@link ToggleButton} to bind.
      * @param aBooleanExpression
@@ -119,157 +119,20 @@ public class FxControlUtils {
     }
 
     /**
-     * @return The URI path to the base style sheet.
-     */
-    public static String getBaseStyleSheet() {
-        return "view/BaseStyle.css";
-    }
-
-    /**
-     * @return The URI path to the loadout style sheet.
-     */
-    public static String getLoadoutStyleSheet() {
-        return "view/LoadoutStyle.css";
-    }
-
-    /**
-     * Will load a FXML file and set the argument as the controller. The name of the FXML file is determined from the
-     * class name of the controller passed in as argument. The FXML file is sought for under resources: "view/".
-     * 
-     * This is a convenient way of creating a custom control. Example:
-     * 
-     * <pre>
-     * class MyControl extends BorderPane{ // Must inherit from the root type in the FXML file.
-     *     public MyControl{
-     *         loadFxmlControl(this);
-     *     }
-     * }
-     * 
-     * View in: "view/MyControl.fxml", must use 'fx:root' construct.
-     * </pre>
-     * 
-     * @param aControl
-     *            The controller.
-     */
-    public static void loadFxmlControl(final Object aControl) {
-        final String fxmlFile = "view/" + aControl.getClass().getSimpleName() + ".fxml";
-        URL fxmlResource = ClassLoader.getSystemClassLoader().getResource(fxmlFile);
-        if (null == fxmlResource) {
-            throw new IllegalArgumentException("Unable to load FXML file: " + fxmlFile);
-        }
-        final FXMLLoader fxmlLoader = new FXMLLoader(fxmlResource);
-        fxmlLoader.setControllerFactory((aClass) -> aControl);
-        fxmlLoader.setRoot(aControl);
-        try {
-            fxmlLoader.load();
-        }
-        catch (final IOException e) {
-            // Failure to load XML is a program error and cannot be recovered from, promote to unchecked.
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Sets up a {@link TreeView} to show garage contents.
-     * 
-     * @param aTreeView
-     *            The {@link TreeView} to set up.
-     * @param aRoot
-     *            The root {@link GarageDirectory} of {@link Garage} to show.
-     * @param aXBar
-     *            A {@link MessageXBar} to listen to changes and to send updates on.
-     * @param aStack
-     *            A {@link CommandStack} to use for executing changes to the garage on.
-     * @param aShowValues
-     *            <code>true</code> if the loadouts or dropships should be shown in the tree.
-     */
-    public static <T extends NamedObject> void setupGarageTree(TreeView<GaragePath<T>> aTreeView,
-            GarageDirectory<T> aRoot, MessageXBar aXBar, CommandStack aStack, boolean aShowValues) {
-        aTreeView.setRoot(new GarageTreeItem<>(aXBar, new GaragePath<T>(null, aRoot), aShowValues));
-        aTreeView.getRoot().setExpanded(true);
-        aTreeView.setShowRoot(true);
-        aTreeView.setCellFactory(aView -> new GarageTreeCell<>(aXBar, aStack, aTreeView));
-        aTreeView.setEditable(true);
-    }
-
-    /**
-     * Sets up a stage with the given {@link Parent} as {@link Scene} root and applies styling to the stage.
-     * 
-     * @param aStage
-     *            The stage to set up.
-     * @param aRoot
-     *            The scene root.
-     * @param aWindowState
-     *            A {@link WindowState} that contains the current status of the custom stage decorations.
-     */
-    public static void setupStage(final Stage aStage, final Region aRoot, WindowState aWindowState) {
-
-        StyleManager.addClass(aRoot, StyleManager.CLASS_DECOR_ROOT);
-
-        Scene scene = new Scene(aRoot);
-        scene.setFill(Color.TRANSPARENT);
-        scene.addEventFilter(MouseEvent.MOUSE_MOVED, aEvent -> {
-            aWindowState.onMouseMoved(aEvent);
-        });
-        scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, aEvent -> {
-            aWindowState.onMouseDragged(aEvent);
-        });
-        scene.addEventFilter(MouseEvent.MOUSE_CLICKED, aEvent -> {
-            aWindowState.onMouseClicked(aEvent);
-        });
-
-        aStage.initStyle(StageStyle.TRANSPARENT);
-        aStage.getIcons().add(new Image(ClassLoader.getSystemClassLoader().getResourceAsStream("icon.png")));
-        aStage.setScene(scene);
-        aStage.sizeToScene();
-        aStage.show();
-        aStage.toFront();
-
-        final Orientation bias = aRoot.getContentBias();
-        final double minWidth;
-        final double minHeight;
-        if (bias == Orientation.VERTICAL) {
-            minHeight = aRoot.minHeight(-1);
-            minWidth = aRoot.minWidth(minHeight);
-        }
-        else {
-            minWidth = aRoot.minWidth(-1);
-            minHeight = aRoot.minHeight(minWidth);
-        }
-        aStage.setMinWidth(minWidth);
-        aStage.setMinHeight(minHeight);
-    }
-
-    /**
-     * Sets up a {@link ToggleButton} to show different text depending on if it is selected or not.
-     * 
-     * @param aButton
-     *            The {@link ToggleButton} to set up.
-     * @param aSelected
-     *            The text to show if the toggle is selected.
-     * @param aUnSelected
-     *            The text to show if the toggle is unselected.
-     */
-    public static void setupToggleText(ToggleButton aButton, String aSelected, String aUnSelected) {
-        StringBinding textBinding = Bindings.when(aButton.selectedProperty()).then(aSelected).otherwise(aUnSelected);
-        aButton.textProperty().bind(textBinding);
-    }
-
-    /**
      * Fixes issues with the JavaFX spinner class:
-     * 
+     *
      * <ul>
      * <li>Commit edit value on focus lost.</li>
      * <li>Revert to last valid on invalid input.</li>
      * </ul>
-     * 
+     *
      * @param aSpinner
      *            The spinner to adjust.
      */
     public static <T> void fixSpinner(Spinner<T> aSpinner) {
 
-        SpinnerValueFactory<T> factory = aSpinner.getValueFactory();
-        TextFormatter<T> formatter = new TextFormatter<>(factory.getConverter(), factory.getValue());
+        final SpinnerValueFactory<T> factory = aSpinner.getValueFactory();
+        final TextFormatter<T> formatter = new TextFormatter<>(factory.getConverter(), factory.getValue());
         aSpinner.getEditor().setTextFormatter(formatter);
         factory.valueProperty().bindBidirectional(formatter.valueProperty());
 
@@ -302,5 +165,151 @@ public class FxControlUtils {
                 aTextField.getOnAction().handle(new ActionEvent(aTextField, aTextField));
             }
         });
+    }
+
+    /**
+     * @return The URI path to the base style sheet.
+     */
+    public static String getBaseStyleSheet() {
+        return "view/BaseStyle.css";
+    }
+
+    /**
+     * @return The URI path to the loadout style sheet.
+     */
+    public static String getLoadoutStyleSheet() {
+        return "view/LoadoutStyle.css";
+    }
+
+    /**
+     * Will load a FXML file and set the argument as the controller. The name of the FXML file is determined from the
+     * class name of the controller passed in as argument. The FXML file is sought for under resources: "view/".
+     *
+     * This is a convenient way of creating a custom control. Example:
+     *
+     * <pre>
+     * class MyControl extends BorderPane{ // Must inherit from the root type in the FXML file.
+     *     public MyControl{
+     *         loadFxmlControl(this);
+     *     }
+     * }
+     *
+     * View in: "view/MyControl.fxml", must use 'fx:root' construct.
+     * </pre>
+     *
+     * @param aControl
+     *            The controller.
+     */
+    public static void loadFxmlControl(final Object aControl) {
+        final String fxmlFile = "view/" + aControl.getClass().getSimpleName() + ".fxml";
+        final URL fxmlResource = ClassLoader.getSystemClassLoader().getResource(fxmlFile);
+        if (null == fxmlResource) {
+            throw new IllegalArgumentException("Unable to load FXML file: " + fxmlFile);
+        }
+        final FXMLLoader fxmlLoader = new FXMLLoader(fxmlResource);
+        fxmlLoader.setControllerFactory((aClass) -> aControl);
+        fxmlLoader.setRoot(aControl);
+        try {
+            fxmlLoader.load();
+        }
+        catch (final IOException e) {
+            // Failure to load XML is a program error and cannot be recovered from, promote to unchecked.
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Sets up a {@link TreeView} to show garage contents.
+     *
+     * @param aTreeView
+     *            The {@link TreeView} to set up.
+     * @param aRoot
+     *            The root {@link GarageDirectory} of {@link Garage} to show.
+     * @param aXBar
+     *            A {@link MessageXBar} to listen to changes and to send updates on.
+     * @param aStack
+     *            A {@link CommandStack} to use for executing changes to the garage on.
+     * @param aShowValues
+     *            <code>true</code> if the loadouts or dropships should be shown in the tree.
+     */
+    public static <T extends NamedObject> void setupGarageTree(TreeView<GaragePath<T>> aTreeView,
+            GarageDirectory<T> aRoot, MessageXBar aXBar, CommandStack aStack, boolean aShowValues) {
+        aTreeView.setRoot(new GarageTreeItem<>(aXBar, new GaragePath<T>(null, aRoot), aShowValues));
+        aTreeView.getRoot().setExpanded(true);
+        aTreeView.setShowRoot(true);
+        aTreeView.setCellFactory(aView -> new GarageTreeCell<>(aXBar, aStack, aTreeView));
+        aTreeView.setEditable(true);
+    }
+
+    /**
+     * Sets up a stage with the given {@link Parent} as {@link Scene} root and applies styling to the stage.
+     *
+     * @param aStage
+     *            The stage to set up.
+     * @param aRoot
+     *            The scene root.
+     * @param aWindowState
+     *            A {@link WindowState} that contains the current status of the custom stage decorations.
+     */
+    public static void setupStage(final Stage aStage, final Region aRoot, WindowState aWindowState) {
+
+        StyleManager.addClass(aRoot, StyleManager.CLASS_DECOR_ROOT);
+
+        final Scene scene = new Scene(aRoot);
+        scene.setFill(Color.TRANSPARENT);
+        scene.addEventFilter(MouseEvent.MOUSE_MOVED, aEvent -> {
+            aWindowState.onMouseMoved(aEvent);
+        });
+        scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, aEvent -> {
+            aWindowState.onMouseDragged(aEvent);
+        });
+        scene.addEventFilter(MouseEvent.MOUSE_CLICKED, aEvent -> {
+            aWindowState.onMouseClicked(aEvent);
+        });
+
+        aStage.initStyle(StageStyle.TRANSPARENT);
+        aStage.getIcons().add(new Image(ClassLoader.getSystemClassLoader().getResourceAsStream("icon.png")));
+        aStage.setScene(scene);
+        aStage.sizeToScene();
+        aStage.show();
+        aStage.toFront();
+
+        if (aStage.getY() < 0) {
+            aStage.setY(0);
+        }
+
+        if (aStage.getX() < 0) {
+            aStage.setX(0);
+        }
+
+        final Orientation bias = aRoot.getContentBias();
+        final double minWidth;
+        final double minHeight;
+        if (bias == Orientation.VERTICAL) {
+            minHeight = aRoot.minHeight(-1);
+            minWidth = aRoot.minWidth(minHeight);
+        }
+        else {
+            minWidth = aRoot.minWidth(-1);
+            minHeight = aRoot.minHeight(minWidth);
+        }
+        aStage.setMinWidth(minWidth);
+        aStage.setMinHeight(minHeight);
+    }
+
+    /**
+     * Sets up a {@link ToggleButton} to show different text depending on if it is selected or not.
+     *
+     * @param aButton
+     *            The {@link ToggleButton} to set up.
+     * @param aSelected
+     *            The text to show if the toggle is selected.
+     * @param aUnSelected
+     *            The text to show if the toggle is unselected.
+     */
+    public static void setupToggleText(ToggleButton aButton, String aSelected, String aUnSelected) {
+        final StringBinding textBinding = Bindings.when(aButton.selectedProperty()).then(aSelected)
+                .otherwise(aUnSelected);
+        aButton.textProperty().bind(textBinding);
     }
 }
