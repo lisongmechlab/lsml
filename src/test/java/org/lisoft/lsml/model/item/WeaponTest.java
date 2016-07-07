eb52d59af38141f6362ed592857a050eb30d1976
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2016-07-07 21:30:38 +0200

    Adding unit tests to Weapon.

--- src/test/java/org/lisoft/lsml/model/item/WeaponTest.java
+++ src/test/java/org/lisoft/lsml/model/item/WeaponTest.java
@@ -22,36 +22,110 @@ package org.lisoft.lsml.model.item;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.lisoft.lsml.model.chassi.HardPointType;
 import org.lisoft.lsml.model.datacache.ItemDB;
 import org.lisoft.lsml.model.datacache.ModifiersDB;
 import org.lisoft.lsml.model.datacache.PilotModuleDB;
+import org.lisoft.lsml.model.modifiers.Attribute;
 import org.lisoft.lsml.model.modifiers.Modifier;
 import org.lisoft.lsml.model.modifiers.ModifierDescription;
 
 public class WeaponTest {
+    private static final String NON_EXISTENT_WEAPON_STAT = "x";
 
+    @SuppressWarnings("unused")
     @Test
-    public void testInequality() {
-        MissileWeapon lrm10 = (MissileWeapon) ItemDB.lookup("LRM 10");
-        MissileWeapon lrm15 = (MissileWeapon) ItemDB.lookup("LRM 15");
-        assertFalse(lrm10.equals(lrm15));
+    public void testConstruction() {
+        final String aName = "name";
+        final String aDesc = "desc";
+        final String aMwoName = "mwo";
+        final int aMwoId = 10;
+        final int aSlots = 11;
+        final double aTons = 12;
+        final HardPointType aHardPointType = HardPointType.AMS;
+        final int aHP = 13;
+        final Faction aFaction = Faction.CLAN;
+
+        final List<String> selectors = Arrays.asList(aMwoName);
+        final int heat = 30;
+        final int cooldown = 31;
+        final int rangeZero = 32;
+        final int rangeMin = 33;
+        final int rangeLong = 34;
+        final int rangeMax = 35;
+        final Attribute aHeat = new Attribute(heat, selectors, ModifierDescription.SPEC_WEAPON_HEAT);
+        final Attribute aCooldown = new Attribute(cooldown, selectors, ModifierDescription.SPEC_WEAPON_COOL_DOWN);
+        final Attribute aRangeZero = new Attribute(rangeZero, selectors, ModifierDescription.SPEC_WEAPON_RANGE_ZERO);
+        final Attribute aRangeMin = new Attribute(rangeMin, selectors, ModifierDescription.SPEC_WEAPON_RANGE_MIN);
+        final Attribute aRangeLong = new Attribute(rangeLong, selectors, ModifierDescription.SPEC_WEAPON_RANGE_LONG);
+        final Attribute aRangeMax = new Attribute(rangeMax, selectors, ModifierDescription.SPEC_WEAPON_RANGE_MAX);
+
+        final double aFallOffExponent = 14;
+        final int aRoundsPerShot = 15;
+        final double aDamagePerProjectile = 16;
+        final int aProjectilesPerRound = 17;
+        final int projectileSpeed = 36;
+        final Attribute aProjectileSpeed = new Attribute(projectileSpeed, selectors,
+                ModifierDescription.SPEC_WEAPON_PROJECTILE_SPEED);
+        final int aGhostHeatGroupId = 18;
+        final double aGhostHeatMultiplier = 19;
+        final int aGhostHeatMaxFreeAlpha = 20;
+        final double aVolleyDelay = 21;
+        final double aImpulse = 22;
+        final Weapon cut = new Weapon(aName, aDesc, aMwoName, aMwoId, aSlots, aTons, aHardPointType, aHP, aFaction,
+                aHeat, aCooldown, aRangeZero, aRangeMin, aRangeLong, aRangeMax, aFallOffExponent, aRoundsPerShot,
+                aDamagePerProjectile, aProjectilesPerRound, aProjectileSpeed, aGhostHeatGroupId, aGhostHeatMultiplier,
+                aGhostHeatMaxFreeAlpha, aVolleyDelay, aImpulse);
+
+        assertEquals(aName, cut.getName());
+        assertEquals(aDesc, cut.getDescription());
+        assertEquals(aMwoName, cut.getKey());
+        assertEquals(aMwoId, cut.getMwoId());
+        assertEquals(aSlots, cut.getSlots());
+        assertEquals(aTons, cut.getMass(), 0.0);
+        assertEquals(aHardPointType, cut.getHardpointType());
+        assertEquals(aFaction, cut.getFaction());
+
+        assertEquals(heat, cut.getHeat(null), 0.0);
+        assertEquals(cooldown, cut.getCoolDown(null), 0.0);
+        assertEquals(rangeZero, cut.getRangeZero(null), 0.0);
+        assertEquals(rangeMin, cut.getRangeMin(null), 0.0);
+        assertEquals(rangeLong, cut.getRangeLong(null), 0.0);
+        assertEquals(rangeMax, cut.getRangeMax(null), 0.0);
+
+        assertTrue(cut.hasNonLinearFalloff());
+        assertEquals(aRoundsPerShot, cut.getAmmoPerPerShot());
+        assertEquals(aDamagePerProjectile * aProjectilesPerRound * aRoundsPerShot, cut.getDamagePerShot(), 0.0);
+        assertEquals(projectileSpeed, cut.getProjectileSpeed(null), 0.0);
+        assertEquals(aGhostHeatGroupId, cut.getGhostHeatGroup());
+        assertEquals(aGhostHeatMultiplier, cut.getGhostHeatMultiplier(), 0.0);
+        assertEquals(aGhostHeatMaxFreeAlpha, cut.getGhostHeatMaxFreeAlpha());
+        assertEquals(cooldown + aVolleyDelay * (aRoundsPerShot - 1), cut.getSecondsPerShot(null), 0.0);
+        assertEquals(aImpulse, cut.getImpulse(), 0.0);
+
+        try {
+            new Weapon(aName, aDesc, aMwoName, aMwoId, aSlots, aTons, aHardPointType, aHP, aFaction, aHeat, aCooldown,
+                    aRangeZero, aRangeMin, aRangeLong, aRangeMax, aFallOffExponent, 0, aDamagePerProjectile,
+                    aProjectilesPerRound, aProjectileSpeed, aGhostHeatGroupId, aGhostHeatMultiplier,
+                    aGhostHeatMaxFreeAlpha, aVolleyDelay, aImpulse);
+            fail("Expected exception");
+        }
+        catch (final IllegalArgumentException e) {
+            // Expected
+        }
     }
 
     @Test
-    public void testGetDamagePerShot_lpl() throws Exception {
-        Weapon weapon = (Weapon) ItemDB.lookup("LRG PULSE LASER");
-        assertTrue(weapon.getDamagePerShot() > 8);
-    }
-
-    @Test
-    public void testGetDamagePerShot_ml() throws Exception {
-        Weapon weapon = (Weapon) ItemDB.lookup("MEDIUM LASER");
-        assertTrue(weapon.getDamagePerShot() > 4);
+    public void testGetDamagePerShot_gauss() {
+        final Weapon gauss = (Weapon) ItemDB.lookup("GAUSS RIFLE");
+        assertTrue(gauss.getDamagePerShot() > 10);
     }
 
     /**
@@ -59,81 +133,42 @@ public class WeaponTest {
      */
     @Test
     public void testGetDamagePerShot_lb10x() {
-        Weapon lb10xac = (Weapon) ItemDB.lookup("LB 10-X AC");
+        final Weapon lb10xac = (Weapon) ItemDB.lookup("LB 10-X AC");
         assertTrue(lb10xac.getDamagePerShot() > 5);
     }
 
     @Test
-    public void testGetShotsPerVolley_lb10x() {
-        Weapon lb10xac = (Weapon) ItemDB.lookup("LB 10-X AC");
-        assertEquals(1, lb10xac.getAmmoPerPerShot());
+    public void testGetDamagePerShot_lpl() throws Exception {
+        final Weapon weapon = (Weapon) ItemDB.lookup("LRG PULSE LASER");
+        assertTrue(weapon.getDamagePerShot() > 8);
     }
 
     @Test
-    public void testGetDamagePerShot_gauss() {
-        Weapon gauss = (Weapon) ItemDB.lookup("GAUSS RIFLE");
-        assertTrue(gauss.getDamagePerShot() > 10);
+    public void testGetDamagePerShot_ml() throws Exception {
+        final Weapon weapon = (Weapon) ItemDB.lookup("MEDIUM LASER");
+        assertTrue(weapon.getDamagePerShot() > 4);
     }
 
     @Test
     public void testGetHeat_gauss() {
-        Weapon gauss = (Weapon) ItemDB.lookup("GAUSS RIFLE");
+        final Weapon gauss = (Weapon) ItemDB.lookup("GAUSS RIFLE");
         assertEquals(1.0, gauss.getHeat(null), 0.0);
     }
 
     @Test
-    public void testGetSecondsPerShot_gauss() {
-        Weapon gauss = (Weapon) ItemDB.lookup("GAUSS RIFLE");
-        assertEquals(gauss.getCoolDown(null) + 0.75, gauss.getSecondsPerShot(null), 0.0);
-    }
-
-    @Test
-    public void testGetSecondsPerShot_mg() {
-        Weapon mg = (Weapon) ItemDB.lookup("MACHINE GUN");
-        assertTrue(mg.getSecondsPerShot(null) > 0.05);
-    }
-
-    @Test
-    public void testGetRangeMin_ppc() {
-        Weapon ppc = (Weapon) ItemDB.lookup("PPC");
-        assertEquals(90.0, ppc.getRangeMin(null), 0.0);
-    }
-
-    @Test
-    public void testGetRangeMax_ppc() {
-        Weapon ppc = (Weapon) ItemDB.lookup("PPC");
-        assertEquals(1080.0, ppc.getRangeMax(null), 0.0);
-    }
-
-    @Test
-    public void testGetRangeLong_ppc() {
-        Weapon ppc = (Weapon) ItemDB.lookup("PPC");
-        assertEquals(540.0, ppc.getRangeLong(null), 0.0);
-    }
-
-    @Test
-    public void testGetRangeEffectivity_mg() {
-        BallisticWeapon mg = (BallisticWeapon) ItemDB.lookup("MACHINE GUN");
-        assertEquals(1.0, mg.getRangeEffectivity(0, null), 0.0);
-        assertEquals(1.0, mg.getRangeEffectivity(mg.getRangeLong(null), null), 0.1); // High spread on MG
-        assertTrue(0.5 >= mg.getRangeEffectivity((mg.getRangeLong(null) + mg.getRangeMax(null)) / 2, null)); // Spread +
-                                                                                                             // falloff
-        assertEquals(0.0, mg.getRangeEffectivity(mg.getRangeMax(null), null), 0.0);
-    }
-
-    @Test
     public void testGetRangeEffectivity_clrm() {
-        MissileWeapon lrm = (MissileWeapon) ItemDB.lookup("C-LRM 20");
+        final MissileWeapon lrm = (MissileWeapon) ItemDB.lookup("C-LRM 20");
         assertEquals(0.0, lrm.getRangeEffectivity(0, null), 0.0);
         assertEquals(0.444, lrm.getRangeEffectivity(120, null), 0.001);
         assertEquals(1.0, lrm.getRangeEffectivity(180, null), 0.0);
         assertEquals(1.0, lrm.getRangeEffectivity(1000, null), 0.0);
         assertEquals(0.0, lrm.getRangeEffectivity(1000 + Math.ulp(2000), null), 0.0);
+        assertTrue(lrm.hasNonLinearFalloff());
     }
 
     @Test
     public void testGetRangeEffectivity_gaussrifle() {
-        BallisticWeapon gauss = (BallisticWeapon) ItemDB.lookup("GAUSS RIFLE");
+        final BallisticWeapon gauss = (BallisticWeapon) ItemDB.lookup("GAUSS RIFLE");
         assertEquals(1.0, gauss.getRangeEffectivity(0, null), 0.0);
         assertEquals(1.0, gauss.getRangeEffectivity(gauss.getRangeLong(null), null), 0.0);
         assertEquals(0.5, gauss.getRangeEffectivity((gauss.getRangeLong(null) + gauss.getRangeMax(null)) / 2, null),
@@ -142,33 +177,104 @@ public class WeaponTest {
 
         assertTrue(gauss.getRangeEffectivity(750, null) < 0.95);
         assertTrue(gauss.getRangeEffectivity(750, null) > 0.8);
+        assertFalse(gauss.hasNonLinearFalloff());
     }
 
     @Test
+    public void testGetRangeEffectivity_mg() {
+        final BallisticWeapon mg = (BallisticWeapon) ItemDB.lookup("MACHINE GUN");
+        assertEquals(1.0, mg.getRangeEffectivity(0, null), 0.0);
+        assertEquals(1.0, mg.getRangeEffectivity(mg.getRangeLong(null), null), 0.1); // High spread on MG
+        assertTrue(0.5 >= mg.getRangeEffectivity((mg.getRangeLong(null) + mg.getRangeMax(null)) / 2, null)); // Spread +
+                                                                                                             // falloff
+        assertEquals(0.0, mg.getRangeEffectivity(mg.getRangeMax(null), null), 0.0);
+    }
+
+    @Test
+    public void testGetRangeLong_ppc() {
+        final Weapon ppc = (Weapon) ItemDB.lookup("PPC");
+        assertEquals(540.0, ppc.getRangeLong(null), 0.0);
+    }
+
+    @Test
+    public void testGetRangeMax_ppc() {
+        final Weapon ppc = (Weapon) ItemDB.lookup("PPC");
+        assertEquals(1080.0, ppc.getRangeMax(null), 0.0);
+    }
+
+    @Test
+    public void testGetRangeMin_ppc() {
+        final Weapon ppc = (Weapon) ItemDB.lookup("PPC");
+        assertEquals(90.0, ppc.getRangeMin(null), 0.0);
+    }
+
+    @Test
+    public void testGetSecondsPerShot_gauss() {
+        final Weapon gauss = (Weapon) ItemDB.lookup("GAUSS RIFLE");
+        assertEquals(gauss.getCoolDown(null) + 0.75, gauss.getSecondsPerShot(null), 0.0);
+    }
+
+    @Test
+    public void testGetSecondsPerShot_mg() {
+        final Weapon mg = (Weapon) ItemDB.lookup("MACHINE GUN");
+        assertTrue(mg.getSecondsPerShot(null) > 0.05);
+    }
+
+    @Test
+    public void testGetShotsPerVolley_lb10x() {
+        final Weapon lb10xac = (Weapon) ItemDB.lookup("LB 10-X AC");
+        assertEquals(1, lb10xac.getAmmoPerPerShot());
+    }
+
+    @Test
+    public void testGetStat() {
+        final Weapon wpn = (Weapon) ItemDB.lookup("ER PPC");
+        assertEquals(wpn.getDamagePerShot() / wpn.getHeat(null), wpn.getStat("d/h", null), 0.0);
+        assertEquals(wpn.getHeat(null) / wpn.getDamagePerShot(), wpn.getStat("h/d", null), 0.0);
+        assertEquals(wpn.getSecondsPerShot(null) / wpn.getMass(), wpn.getStat("s/t", null), 0.0);
+        assertEquals(wpn.getMass() / wpn.getSecondsPerShot(null), wpn.getStat("t/s", null), 0.0);
+        assertEquals(wpn.getSlots(), wpn.getStat("c", null), 0.0);
+        assertEquals(1.0, wpn.getStat("dsthc/dsthc", null), 0.0);
+    }
+
+    /**
+     * Gauss has low heat test specially
+     */
+    @Test
     public void testGetStat_gauss() {
-        BallisticWeapon gauss = (BallisticWeapon) ItemDB.lookup("GAUSS RIFLE");
+        final BallisticWeapon gauss = (BallisticWeapon) ItemDB.lookup("GAUSS RIFLE");
         assertEquals(gauss.getDamagePerShot() / gauss.getHeat(null), gauss.getStat("d/h", null), 0.0);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetStatFormatErrorDenominator() {
+        final Weapon wpn = (Weapon) ItemDB.lookup("ER PPC");
+        wpn.getStat("/" + NON_EXISTENT_WEAPON_STAT, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetStatFormatErrorNominator() {
+        final Weapon wpn = (Weapon) ItemDB.lookup("ER PPC");
+        wpn.getStat(NON_EXISTENT_WEAPON_STAT, null);
+    }
+
+    /**
+     * When taking the quotient of two stats that are zero we're faced with interpreting 0/0. Although not strictly
+     * mathematically correct, we will interpret x/y as 'x' per 'y' and if 'x' is zero we will output zero if 'y' is
+     * also finite.a
+     */
     @Test
-    public void testRangeModifiers() {
-        Weapon llas = (Weapon) ItemDB.lookup("LARGE LASER");
-        WeaponModule rangeModule = (WeaponModule) PilotModuleDB.lookup("LARGE LASER RANGE 5");
-        ModifierDescription rangelongQuirk1 = ModifiersDB.lookup("islargelaser_longrange_multiplier");
-        ModifierDescription rangemaxQuirk2 = ModifiersDB.lookup("energy_maxrange_multiplier");
-        Modifier rangelong1 = new Modifier(rangelongQuirk1, 0.125);
-        Modifier rangemax2 = new Modifier(rangemaxQuirk2, 0.125);
+    public void testGetStatZeroOverZero() {
+        // AMS has no heat.
+        assertEquals(0.0, ItemDB.AMS.getHeat(null), 0.0);
+        assertEquals(0.0, ItemDB.AMS.getStat("h/h", null), 0.0);
+    }
 
-        List<Modifier> modifiers = new ArrayList<>();
-        modifiers.addAll(rangeModule.getModifiers());
-        modifiers.add(rangelong1);
-        modifiers.add(rangemax2);
-
-        double expectedLongRange = (llas.getRangeLong(null) + 0.0) * (1.0 + 0.125 + 0.1);
-        assertEquals(expectedLongRange, llas.getRangeLong(modifiers), 0.0);
-
-        double expectedMaxRange = (llas.getRangeMax(null) + 0.0) * (1.0 + 0.125 + 0.1);
-        assertEquals(expectedMaxRange, llas.getRangeMax(modifiers), 0.0);
+    @Test
+    public void testInequality() {
+        final MissileWeapon lrm10 = (MissileWeapon) ItemDB.lookup("LRM 10");
+        final MissileWeapon lrm15 = (MissileWeapon) ItemDB.lookup("LRM 15");
+        assertFalse(lrm10.equals(lrm15));
     }
 
     @Test
@@ -181,6 +287,35 @@ public class WeaponTest {
         assertTrue(((Weapon) ItemDB.lookup("C-LB5-X AC")).isLargeBore());
         assertFalse(((Weapon) ItemDB.lookup("MACHINE GUN")).isLargeBore());
         assertFalse(((Weapon) ItemDB.lookup("AMS")).isLargeBore());
+    }
+
+    @Test
+    public void testIsOffensive() {
+        assertTrue(((Weapon) ItemDB.lookup("C-ER PPC")).isOffensive());
+        assertFalse(((Weapon) ItemDB.lookup("AMS")).isOffensive());
+        assertFalse(((Weapon) ItemDB.lookup("C-AMS")).isOffensive());
+
+    }
+
+    @Test
+    public void testRangeModifiers() {
+        final Weapon llas = (Weapon) ItemDB.lookup("LARGE LASER");
+        final WeaponModule rangeModule = (WeaponModule) PilotModuleDB.lookup("LARGE LASER RANGE 5");
+        final ModifierDescription rangelongQuirk1 = ModifiersDB.lookup("islargelaser_longrange_multiplier");
+        final ModifierDescription rangemaxQuirk2 = ModifiersDB.lookup("energy_maxrange_multiplier");
+        final Modifier rangelong1 = new Modifier(rangelongQuirk1, 0.125);
+        final Modifier rangemax2 = new Modifier(rangemaxQuirk2, 0.125);
+
+        final List<Modifier> modifiers = new ArrayList<>();
+        modifiers.addAll(rangeModule.getModifiers());
+        modifiers.add(rangelong1);
+        modifiers.add(rangemax2);
+
+        final double expectedLongRange = (llas.getRangeLong(null) + 0.0) * (1.0 + 0.125 + 0.1);
+        assertEquals(expectedLongRange, llas.getRangeLong(modifiers), 0.0);
+
+        final double expectedMaxRange = (llas.getRangeMax(null) + 0.0) * (1.0 + 0.125 + 0.1);
+        assertEquals(expectedMaxRange, llas.getRangeMax(modifiers), 0.0);
     }
 
 }
