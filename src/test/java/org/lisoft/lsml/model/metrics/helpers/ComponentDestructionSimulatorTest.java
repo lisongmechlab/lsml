ca4673751acaf5d3c6ebd146c8a0b1a61dc350b9
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2017-04-15 13:09:14 +0200

    Fixed failing tests.

--- src/test/java/org/lisoft/lsml/model/metrics/helpers/ComponentDestructionSimulatorTest.java
+++ src/test/java/org/lisoft/lsml/model/metrics/helpers/ComponentDestructionSimulatorTest.java
@@ -21,12 +21,13 @@ package org.lisoft.lsml.model.metrics.helpers;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 import org.lisoft.lsml.model.chassi.ComponentStandard;
-import org.lisoft.lsml.model.datacache.ItemDB;
+import org.lisoft.lsml.model.chassi.HardPointType;
+import org.lisoft.lsml.model.item.Faction;
 import org.lisoft.lsml.model.item.Item;
 import org.lisoft.lsml.model.loadout.ConfiguredComponent;
 import org.lisoft.lsml.model.metrics.CriticalStrikeProbability;
@@ -40,50 +41,56 @@ import org.mockito.Mockito;
 public class ComponentDestructionSimulatorTest {
 
     /**
-     * The AC20 has 18 health and needs two 10-point alphas to be destroyed
+     * The test item has 10 slots, 18 health and needs two 10-point alphas to be destroyed
      */
     @Test
-    public void testAC20Health() {
-        final double partHp = 2; // Only allow one alpha
-        final List<Item> partItems = new ArrayList<>();
-        partItems.add(ItemDB.lookup("AC/20"));
+    public void testDoubleCritToDestroy() {
+        final Item item = makeTestItem(10, 18.0);
+        // Low HP to only allow one alpha before component is destroyed
+        final ConfiguredComponent component = makeTestComponent(2.0, Arrays.asList(item));
+        final ComponentDestructionSimulator cut = new ComponentDestructionSimulator(component);
 
-        final ComponentStandard internalPart = Mockito.mock(ComponentStandard.class);
-        Mockito.when(internalPart.getHitPoints(null)).thenReturn(partHp);
-
-        final ConfiguredComponent part = Mockito.mock(ConfiguredComponent.class);
-        Mockito.when(part.getItemsEquipped()).thenReturn(partItems);
-        Mockito.when(part.getInternalComponent()).thenReturn(internalPart);
-
-        final ComponentDestructionSimulator cut = new ComponentDestructionSimulator(part);
         cut.simulate(null);
 
         // The AC/20 will only explode if there is a double or triple critical hit (14+3%)
         final double P_hit = CriticalStrikeProbability.CRIT_CHANCE.get(1)
                 + CriticalStrikeProbability.CRIT_CHANCE.get(2);
 
-        assertEquals(P_hit, cut.getProbabilityOfDestruction(ItemDB.lookup("AC/20")), 0.0001);
+        assertEquals(P_hit, cut.getProbabilityOfDestruction(item), 0.0001);
     }
 
     /**
-     * The AC20 has 18 health and needs two 10-point alphas to be destroyed.
+     * In this test there is a single item with low HP that will be destroyed by a single crit.
+     *
+     * We take 5 shots and compute the chance that it is destroyed.
+     */
+    @Test
+    public void testSingleCritToDestroy() {
+        final Item item = makeTestItem(2, 7.5);
+        final ConfiguredComponent component = makeTestComponent(42.0, Arrays.asList(item));
+        final ComponentDestructionSimulator cut = new ComponentDestructionSimulator(component);
+
+        cut.simulate(null);
+
+        // There are 5 shots before the item explodes, thus the probability of all of them missing the item is:
+        // 0.58^5, i.e. the probability of the item exploding is: 1 - 0.58^5
+        final double P_hit = 1 - Math.pow(CriticalStrikeProbability.MISS_CHANCE, 5);
+
+        assertEquals(P_hit, cut.getProbabilityOfDestruction(item), 0.0001);
+    }
+
+    /**
+     * The test item has 10 slots and 18 health and needs two 10-point alphas to be destroyed.
      * <p>
      * Test with two shots.
      */
     @Test
-    public void testAC20HealthTwoAlphas() {
-        final double partHp = 12; // Allow two alphas
-        final List<Item> partItems = new ArrayList<>();
-        partItems.add(ItemDB.lookup("AC/20"));
+    public void testTwoHitsToTwoCrits() {
+        final Item item = makeTestItem(10, 18.0);
+        // Give the component 12.0 HP to survive one shot and explode on the second.
+        final ConfiguredComponent component = makeTestComponent(12.0, Arrays.asList(item));
+        final ComponentDestructionSimulator cut = new ComponentDestructionSimulator(component);
 
-        final ComponentStandard internalPart = Mockito.mock(ComponentStandard.class);
-        Mockito.when(internalPart.getHitPoints(null)).thenReturn(partHp);
-
-        final ConfiguredComponent part = Mockito.mock(ConfiguredComponent.class);
-        Mockito.when(part.getItemsEquipped()).thenReturn(partItems);
-        Mockito.when(part.getInternalComponent()).thenReturn(internalPart);
-
-        final ComponentDestructionSimulator cut = new ComponentDestructionSimulator(part);
         cut.simulate(null);
 
         // The AC/20 will only explode if it is hit twice or more
@@ -92,36 +99,22 @@ public class ComponentDestructionSimulatorTest {
         // Second shit, first one was a single hit: Need 1 hits, ie 0.25*0.42
         // Total probability: 0.17*1.0 + 0.25*0.42 + 0.58*0.17
         final double P_hit = 0.17 * 1.0 + 0.25 * 0.42 + 0.58 * 0.17;
-        assertEquals(P_hit, cut.getProbabilityOfDestruction(ItemDB.lookup("AC/20")), 0.0001);
+        assertEquals(P_hit, cut.getProbabilityOfDestruction(item), 0.0001);
     }
 
     // TODO: Add test to test with modifiers
 
-    @Test
-    public void testSoloComponent() {
-        final double partHp = 42;
-        final List<Item> partItems = new ArrayList<>();
-        partItems.add(ItemDB.BAP);
+    private ConfiguredComponent makeTestComponent(double aHP, List<Item> aItems) {
+        final ComponentStandard componentStructure = Mockito.mock(ComponentStandard.class);
+        Mockito.when(componentStructure.getHitPoints(null)).thenReturn(aHP);
 
-        final ComponentStandard internalPart = Mockito.mock(ComponentStandard.class);
-        Mockito.when(internalPart.getHitPoints(null)).thenReturn(partHp);
+        final ConfiguredComponent component = Mockito.mock(ConfiguredComponent.class);
+        Mockito.when(component.getItemsEquipped()).thenReturn(aItems);
+        Mockito.when(component.getInternalComponent()).thenReturn(componentStructure);
+        return component;
+    }
 
-        final ConfiguredComponent part = Mockito.mock(ConfiguredComponent.class);
-        Mockito.when(part.getItemsEquipped()).thenReturn(partItems);
-        Mockito.when(part.getInternalComponent()).thenReturn(internalPart);
-
-        final ComponentDestructionSimulator cut = new ComponentDestructionSimulator(part);
-        cut.simulate(null);
-
-        // There are 5 shots before the item explodes, thus the probability of all of them missing the item is:
-        // 0.58^5, i.e. the probability of the item exploding is: 1 - 0.58^5
-        double P_miss = 1.0;
-        for (int i = 0; i < CriticalStrikeProbability.CRIT_CHANCE.size(); ++i) {
-            P_miss -= CriticalStrikeProbability.CRIT_CHANCE.get(i);
-        }
-
-        final double P_hit = 1 - Math.pow(P_miss, 5);
-
-        assertEquals(P_hit, cut.getProbabilityOfDestruction(ItemDB.BAP), 0.0001);
+    private Item makeTestItem(int aSlots, double aHP) {
+        return new Item(null, null, null, 0, aSlots, 0, HardPointType.NONE, aHP, Faction.INNERSPHERE, null, null);
     }
 }
