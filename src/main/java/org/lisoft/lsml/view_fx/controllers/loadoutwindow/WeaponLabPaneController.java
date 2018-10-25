3b7cea2d56802ee9d4b35f3f6b176fab4a38eb7f
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2018-10-25 19:15:22 +0200

    Fixing #704. Real cause was the y-axis lower bound being set to 1.0 and then the upper bound was computed to 0.0 and the graph got messed up. Changing lower bound to 0.0 and setting minimum upper bound to 1.0.

--- src/main/java/org/lisoft/lsml/view_fx/controllers/loadoutwindow/WeaponLabPaneController.java
+++ src/main/java/org/lisoft/lsml/view_fx/controllers/loadoutwindow/WeaponLabPaneController.java
@@ -19,35 +19,58 @@
 //@formatter:on
 package org.lisoft.lsml.view_fx.controllers.loadoutwindow;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map.Entry;
+import java.util.SortedMap;
 
-import javax.inject.*;
+import javax.inject.Inject;
+import javax.inject.Named;
 
-import org.lisoft.lsml.messages.*;
+import org.lisoft.lsml.messages.ApplicationMessage;
+import org.lisoft.lsml.messages.ItemMessage;
+import org.lisoft.lsml.messages.LoadoutMessage;
 import org.lisoft.lsml.messages.LoadoutMessage.Type;
-import org.lisoft.lsml.model.graphs.*;
+import org.lisoft.lsml.messages.Message;
+import org.lisoft.lsml.messages.MessageReceiver;
+import org.lisoft.lsml.messages.MessageXBar;
+import org.lisoft.lsml.model.graphs.AlphaStrikeGraphModel;
+import org.lisoft.lsml.model.graphs.DamageGraphModel;
+import org.lisoft.lsml.model.graphs.MaxDpsGraphModel;
+import org.lisoft.lsml.model.graphs.SustainedDpsGraphModel;
 import org.lisoft.lsml.model.item.Weapon;
-import org.lisoft.lsml.model.loadout.*;
+import org.lisoft.lsml.model.loadout.Loadout;
+import org.lisoft.lsml.model.loadout.WeaponGroups;
 import org.lisoft.lsml.util.Pair;
 import org.lisoft.lsml.view_fx.controllers.AbstractFXController;
 import org.lisoft.lsml.view_fx.controls.FixedRowsTableView;
 import org.lisoft.lsml.view_fx.properties.LoadoutMetrics;
 import org.lisoft.lsml.view_fx.style.StyleManager;
-import org.lisoft.lsml.view_fx.util.*;
+import org.lisoft.lsml.view_fx.util.FxControlUtils;
+import org.lisoft.lsml.view_fx.util.FxTableUtils;
 
 import javafx.application.Platform;
-import javafx.beans.binding.*;
-import javafx.beans.property.*;
-import javafx.collections.*;
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.binding.ListBinding;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Side;
-import javafx.scene.chart.*;
-import javafx.scene.chart.XYChart.*;
-import javafx.scene.control.*;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.StackedAreaChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TitledPane;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 
 /**
  * A control that displays stats for a weapon group.
@@ -55,6 +78,7 @@ import javafx.scene.layout.*;
  * @author Li Song
  */
 public class WeaponLabPaneController extends AbstractFXController implements MessageReceiver {
+    private static final double MINIMUM_Y_AXIS_UPPER_BOUND = 1.0;
 
     static public class WeaponState {
         private final BooleanProperty[] groupState;
@@ -222,7 +246,7 @@ public class WeaponLabPaneController extends AbstractFXController implements Mes
         final NumberAxis yAxis = (NumberAxis) yAxisRaw;
 
         xAxis.setLowerBound(1.0);
-        yAxis.setLowerBound(1.0);
+        yAxis.setLowerBound(0.0);
 
         final double xStep = 200;
         final double yStep = 2.5;
@@ -258,7 +282,7 @@ public class WeaponLabPaneController extends AbstractFXController implements Mes
                     maxY += series.getData().stream().map(x -> x.getYValue()).max(Comparator.naturalOrder())
                             .orElse(0.0);
                 }
-                return Math.ceil(maxY / yStep) * yStep;
+                return Math.max(MINIMUM_Y_AXIS_UPPER_BOUND, Math.ceil(maxY / yStep) * yStep);
             }
         });
         return dataBinding;
