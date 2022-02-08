e4ee1ea0db75eff9df7e7edc6246267a1318f5de
Li Song <5306929+LiSongMWO@users.noreply.github.com>
2022-02-08 01:10:48 +0100

    Fixes multiple issues with ghost heat calculation. Fixes #764. In particular: Maximum free alpha is now correctly calculated and the right multiplier is used for the penalty.

--- src/main/java/org/lisoft/lsml/model/metrics/GhostHeat.java
+++ src/main/java/org/lisoft/lsml/model/metrics/GhostHeat.java
@@ -19,16 +19,14 @@
 //@formatter:on
 package org.lisoft.lsml.model.metrics;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import org.lisoft.lsml.model.item.Weapon;
 import org.lisoft.lsml.model.loadout.Loadout;
 import org.lisoft.lsml.model.modifiers.Modifier;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+import static java.util.Comparator.naturalOrder;
 
 /**
  * This {@link Metric} calculates the total ghost heat penalty for an alpha strike from a loadout.
@@ -36,16 +34,14 @@ import org.lisoft.lsml.model.modifiers.Modifier;
  * @author Li Song
  */
 public class GhostHeat implements Metric {
-    private static final double HEAT_SCALE[] = { 0, 0, 0.08, 0.18, 0.30, 0.45, 0.60, 0.80, 1.10, 1.50, 2.00, 3.00,
-            5.00 };
+    private static final double[] HEAT_SCALE = {0, 0, 0.08, 0.18, 0.30, 0.45, 0.60, 0.80, 1.10, 1.50, 2.00, 3.00, 5.00};
     private final Loadout loadout;
     private final int weaponGroup;
 
     /**
      * Constructs a new {@link GhostHeat} metric that will calculate the ghost heat for the entire loadout.
      *
-     * @param aLoadout
-     *            The loadout to calculate for.
+     * @param aLoadout The loadout to calculate for.
      */
     public GhostHeat(Loadout aLoadout) {
         this(aLoadout, -1);
@@ -54,10 +50,8 @@ public class GhostHeat implements Metric {
     /**
      * Constructs a new {@link GhostHeat} metric that calculates the ghost heat for a given weapon group.
      *
-     * @param aLoadout
-     *            The loadout to calculate for.
-     * @param aGroup
-     *            The weapon group to calculate for.
+     * @param aLoadout The loadout to calculate for.
+     * @param aGroup   The weapon group to calculate for.
      */
     public GhostHeat(Loadout aLoadout, int aGroup) {
         loadout = aLoadout;
@@ -73,54 +67,49 @@ public class GhostHeat implements Metric {
         final Iterable<Weapon> weapons;
         if (weaponGroup < 0) {
             weapons = loadout.items(Weapon.class);
-        }
-        else {
+        } else {
             weapons = loadout.getWeaponGroups().getWeapons(weaponGroup, loadout);
         }
 
         for (final Weapon weapon : weapons) {
             final int group = weapon.getGhostHeatGroup();
             if (group == 0) {
-                ungroupedWeapons.put(weapon, ungroupedWeapons.getOrDefault(weapons, 0) + 1);
-            }
-            else if (group > 0) {
+                ungroupedWeapons.put(weapon, ungroupedWeapons.getOrDefault(weapon, 0) + 1);
+            } else if (group > 0) {
                 groups.computeIfAbsent(group, ArrayList::new).add(weapon);
             }
         }
 
         double penalty = 0;
         for (final Entry<Weapon, Integer> entry : ungroupedWeapons.entrySet()) {
-            penalty += calculatePenalty(entry.getKey(), entry.getValue(), modifiers);
+            Weapon weapon = entry.getKey();
+            int count = entry.getValue();
+            penalty += calculatePenalty(weapon.getHeat(modifiers), weapon.getGhostHeatMultiplier(),
+                                        weapon.getGhostHeatMaxFreeAlpha(modifiers), count);
         }
 
         // XXX: http://mwomercs.com/forums/topic/127904-heat-scale-the-maths/ is not completely
         // clear on this. We interpret the post to mean that for the purpose of ghost heat, every weapon
-        // in the linked group is equal to the weapon with highest base heat.
+        // in the linked group is equal to the weapon with the highest base heat.
         // Update(2018-10-28): It appears the post was amended after the fact which confirms the above
         // assumption
         for (final List<Weapon> group : groups.values()) {
-            double maxbaseheat = Double.NEGATIVE_INFINITY;
-            Weapon maxweapon = null;
-            for (final Weapon w : group) {
-                // XXX: It's not certain that heat applied from modules will affect the base heat value
-                // for the purpose of selecting the weapon with the highest heat for computing the penalty.
-                // But we do this as it gives us a pessimistic value rather than an optimistic one.
-                if (w.getHeat(modifiers) > maxbaseheat) {
-                    maxbaseheat = w.getHeat(modifiers);
-                    maxweapon = w;
-                }
-            }
-            penalty += calculatePenalty(maxweapon, group.size(), modifiers);
+            @SuppressWarnings("OptionalGetWithoutIsPresent") // Always present
+            double maxHeat = group.stream().map(w -> w.getHeat(modifiers)).max(naturalOrder()).get();
+            @SuppressWarnings("OptionalGetWithoutIsPresent") // Always present
+            double maxMultiplier = group.stream().map(Weapon::getGhostHeatMultiplier).max(naturalOrder()).get();
+            @SuppressWarnings("OptionalGetWithoutIsPresent") // Always present
+            int maxFreeAlpha = group.stream().map(w -> w.getGhostHeatMaxFreeAlpha(modifiers)).min(naturalOrder()).get();
+            penalty += calculatePenalty(maxHeat, maxMultiplier, maxFreeAlpha, group.size());
         }
         return penalty;
     }
 
-    private double calculatePenalty(Weapon aWeapon, int aCount, Collection<Modifier> aModifiers) {
+    private double calculatePenalty(double aHeat, double aMultiplier, int aMaxFreeAlpha, int aCount) {
         double penalty = 0;
         int count = aCount;
-        while (count > aWeapon.getGhostHeatMaxFreeAlpha(aModifiers)) {
-            penalty += HEAT_SCALE[Math.min(count, HEAT_SCALE.length - 1)] * aWeapon.getGhostHeatMultiplier()
-                    * aWeapon.getHeat(aModifiers);
+        while (count > aMaxFreeAlpha) {
+            penalty += HEAT_SCALE[Math.min(count, HEAT_SCALE.length - 1)] * aMultiplier * aHeat;
             count--;
         }
         return penalty;
--- src/test/java/org/lisoft/lsml/model/metrics/GhostHeatTest.java
+++ src/test/java/org/lisoft/lsml/model/metrics/GhostHeatTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.lisoft.lsml.model.database.ItemDB;
 import org.lisoft.lsml.model.helpers.MockLoadoutContainer;
+import org.lisoft.lsml.model.item.BallisticWeapon;
 import org.lisoft.lsml.model.item.EnergyWeapon;
 import org.lisoft.lsml.model.item.Weapon;
 import org.lisoft.lsml.model.modifiers.Modifier;
@@ -23,10 +24,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class GhostHeatTest {
+    private final MockLoadoutContainer mlc = new MockLoadoutContainer();
     private final EnergyWeapon ppc;
     private final List<Weapon> weapons = new ArrayList<>();
-    private final MockLoadoutContainer mlc = new MockLoadoutContainer();
-
     private GhostHeat cut;
 
     public GhostHeatTest() {
@@ -107,59 +107,15 @@ public class GhostHeatTest {
         weapons.add(realPPC);
 
         final ModifierDescription hslDescription = new ModifierDescription("", "", Operation.ADD,
-                ModifierDescription.SEL_ALL, ModifierDescription.SPEC_WEAPON_MAX_FREE_ALPHA,
-                ModifierType.POSITIVE_GOOD);
+                                                                           ModifierDescription.SEL_ALL,
+                                                                           ModifierDescription.SPEC_WEAPON_MAX_FREE_ALPHA,
+                                                                           ModifierType.POSITIVE_GOOD);
         final Modifier hslQuirk = new Modifier(hslDescription, 1);
         final List<Modifier> modifiers = Collections.singletonList(hslQuirk);
         when(mlc.loadout.getAllModifiers()).thenReturn(modifiers);
 
         final double result = cut.calculate();
         assertEquals(0.0, result, 0.01);
-    }
-
-    @Test
-    public void testCalculate_linkedMixedGroup() throws Exception {
-        // Example from: http://mwomercs.com/forums/topic/127904-heat-scale-the-maths/
-
-        final Weapon lplas = (Weapon) ItemDB.lookup("LRG PULSE LASER");
-        final Weapon erllas = (Weapon) ItemDB.lookup("ER LARGE LASER");
-        final Weapon llas = (Weapon) ItemDB.lookup("LARGE LASER");
-
-        weapons.add(erllas);
-        weapons.add(llas);
-        weapons.add(lplas);
-        weapons.add(llas);
-        weapons.add(llas);
-
-        Weapon max = lplas.getHeat(null) > erllas.getHeat(null) ? lplas : erllas;
-        max = max.getHeat(null) > llas.getHeat(null) ? max : llas;
-
-        final double result = cut.calculate();
-        assertEquals(max.getGhostHeatMultiplier() * max.getHeat(null) * (0.30 + 0.45), result, 0.0001);
-    }
-
-    @Test
-    public void testCalculate_unlinkedMixedGroup() throws Exception {
-        // Example from: http://mwomercs.com/forums/topic/127904-heat-scale-the-maths/
-
-        final Weapon ac20 = (Weapon) ItemDB.lookup("AC/20");
-        final Weapon mlas = (Weapon) ItemDB.lookup("MEDIUM LASER");
-        weapons.add(ac20);
-        weapons.add(mlas);
-        weapons.add(ac20);
-        weapons.add(mlas);
-        weapons.add(mlas);
-        weapons.add(mlas);
-        weapons.add(mlas);
-        weapons.add(mlas);
-        weapons.add(mlas);
-        weapons.add(mlas);
-        weapons.add(mlas);
-
-        final double result = cut.calculate();
-        final double ac20penalty = ac20.getGhostHeatMultiplier() * ac20.getHeat(null) * 0.08;
-        final double mlaspenalty = mlas.getGhostHeatMultiplier() * mlas.getHeat(null) * (0.80 + 1.10 + 1.50);
-        assertEquals(ac20penalty + mlaspenalty, result, 0.0001);
     }
 
     @Test
@@ -199,5 +155,148 @@ public class GhostHeatTest {
         cut = new GhostHeat(mlc.loadout, aGroup);
         final double result = cut.calculate();
         assertEquals(31.92, result, 0.01); // Adjusting for new base head (9.5)
+    }
+
+    @Test
+    public void testCalculate_linkedMixedGroup() throws Exception {
+        // Example from: http://mwomercs.com/forums/topic/127904-heat-scale-the-maths/
+
+        final Weapon lplas = (Weapon) ItemDB.lookup("LRG PULSE LASER");
+        final Weapon erllas = (Weapon) ItemDB.lookup("ER LARGE LASER");
+        final Weapon llas = (Weapon) ItemDB.lookup("LARGE LASER");
+
+        weapons.add(erllas);
+        weapons.add(llas);
+        weapons.add(lplas);
+        weapons.add(llas);
+        weapons.add(llas);
+
+        Weapon max = lplas.getHeat(null) > erllas.getHeat(null) ? lplas : erllas;
+        max = max.getHeat(null) > llas.getHeat(null) ? max : llas;
+
+        final double result = cut.calculate();
+        assertEquals(max.getGhostHeatMultiplier() * max.getHeat(null) * (0.30 + 0.45), result, 0.0001);
+    }
+
+    @Test
+    public void testCalculate_linkedMixedMaxFreeAlpha() {
+        // Based on sciencing in the in-game mechlab, the lowest maxFreeAlpha of any weapon applies to the whole group.
+        EnergyWeapon lightppc = mock(EnergyWeapon.class);
+        when(lightppc.getHeat(any())).thenReturn(4.5);
+        when(lightppc.getGhostHeatMaxFreeAlpha(any())).thenReturn(3);
+        when(lightppc.getGhostHeatMultiplier()).thenReturn(7.0);
+        when(lightppc.getGhostHeatGroup()).thenReturn(4);
+
+        EnergyWeapon erppc = mock(EnergyWeapon.class);
+        when(erppc.getHeat(any())).thenReturn(12.0);
+        when(erppc.getGhostHeatMaxFreeAlpha(any())).thenReturn(2);
+        when(erppc.getGhostHeatMultiplier()).thenReturn(4.0);
+        when(erppc.getGhostHeatGroup()).thenReturn(4);
+
+        weapons.add(lightppc);
+        weapons.add(lightppc);
+        weapons.add(lightppc);
+
+        // Three light PPCs cause no ghost heat
+        assertEquals(0, cut.calculate(), 0.0001);
+
+        weapons.remove(lightppc);
+        weapons.add(erppc);
+
+        // Two Light PPCs and one ER PPC causes ghost heat because the ER PPC brings down the max free alpha of the
+        // group to 2.
+        final double expected = 0.18 * 12 * 7;
+        assertEquals(expected, cut.calculate(), 0.0001);
+    }
+
+    @Test
+    public void testCalculate_linkedMixedMultiplier() {
+        EnergyWeapon erppc = mock(EnergyWeapon.class);
+        when(erppc.getHeat(any())).thenReturn(14.5);
+        when(erppc.getGhostHeatMaxFreeAlpha(any())).thenReturn(2);
+        when(erppc.getGhostHeatMultiplier()).thenReturn(7.0);
+        when(erppc.getGhostHeatGroup()).thenReturn(4);
+
+        BallisticWeapon gauss = mock(BallisticWeapon.class);
+        when(gauss.getHeat(any())).thenReturn(1.0);
+        when(gauss.getGhostHeatMaxFreeAlpha(any())).thenReturn(2);
+        when(gauss.getGhostHeatMultiplier()).thenReturn(8.5);
+        when(gauss.getGhostHeatGroup()).thenReturn(4);
+
+        weapons.add(erppc);
+        weapons.add(erppc);
+        weapons.add(gauss);
+
+        final double result = cut.calculate();
+        final double expected = 0.18 * 14.5 * 8.5;
+        assertEquals(expected, result, 0.0001);
+    }
+
+    @Test
+    public void testCalculate_unGrouped() {
+        // Weapons that do not have a group specified are treated as their own group
+
+        BallisticWeapon ac20 = mock(BallisticWeapon.class);
+        when(ac20.getHeat(any())).thenReturn(5.0);
+        when(ac20.getGhostHeatMaxFreeAlpha(any())).thenReturn(1);
+        when(ac20.getGhostHeatMultiplier()).thenReturn(24.0);
+        when(ac20.getGhostHeatGroup()).thenReturn(0);
+
+        EnergyWeapon mlas = mock(EnergyWeapon.class);
+        when(mlas.getHeat(any())).thenReturn(3.25);
+        when(mlas.getGhostHeatMaxFreeAlpha(any())).thenReturn(6);
+        when(mlas.getGhostHeatMultiplier()).thenReturn(1.0);
+        when(mlas.getGhostHeatGroup()).thenReturn(0);
+
+        weapons.add(ac20);
+        weapons.add(mlas);
+        weapons.add(ac20);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+
+        final double result = cut.calculate();
+        final double ac20Penalty = 24.0 * 5 * 0.08;
+        final double mlasPenalty = 1 * 3.25 * (0.80 + 1.10 + 1.50);
+        assertEquals(ac20Penalty + mlasPenalty, result, 0.0001);
+    }
+
+    @Test
+    public void testCalculate_unlinkedMixedGroup() {
+        // Example from: http://mwomercs.com/forums/topic/127904-heat-scale-the-maths/
+
+        BallisticWeapon ac20 = mock(BallisticWeapon.class);
+        when(ac20.getHeat(any())).thenReturn(5.0);
+        when(ac20.getGhostHeatMaxFreeAlpha(any())).thenReturn(1);
+        when(ac20.getGhostHeatMultiplier()).thenReturn(24.0);
+        when(ac20.getGhostHeatGroup()).thenReturn(9);
+
+        EnergyWeapon mlas = mock(EnergyWeapon.class);
+        when(mlas.getHeat(any())).thenReturn(3.25);
+        when(mlas.getGhostHeatMaxFreeAlpha(any())).thenReturn(6);
+        when(mlas.getGhostHeatMultiplier()).thenReturn(1.0);
+        when(mlas.getGhostHeatGroup()).thenReturn(12);
+
+        weapons.add(ac20);
+        weapons.add(mlas);
+        weapons.add(ac20);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+        weapons.add(mlas);
+
+        final double result = cut.calculate();
+        final double ac20Penalty = 24.0 * 5 * 0.08;
+        final double mlasPenalty = 1 * 3.25 * (0.80 + 1.10 + 1.50);
+        assertEquals(ac20Penalty + mlasPenalty, result, 0.0001);
     }
 }
